d214f29d05968f8971039b5b0f5412f005a9ba4d
Joel <cdiaz_jl@hotmail.com.ar>
2021-07-07 18:15:49 -0500

    #2 Modelo y migración del centro de acopio

---
+++ app/Models/Collection_Center_Plastic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Collection_Center_Plastic extends Model
+{
+    //use HasFactory;
+    protected $fillable = [
+        'manager_name',
+        'manager_last_name',
+        'manager_email',
+        'manager_password',
+        'manager_cellphone',
+        'manager_neighborhood',
+        'manager_address',
+        'manager_picture',
+        'manager_latitude',
+        'manager_longitude'];
+}
