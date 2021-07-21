0bb1585994e794a908ef64932a0adec173713b32
rubenesteban <rubenmaya753@yahoo.com>
2021-07-20 19:29:38 -0500

    #8 Seeding Parroquia, Canton, Provincia

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Collection_Center_Plastic;
+use App\Models\Provincia;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\This;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,10 +21,13 @@ class DatabaseSeeder extends Seeder
         Schema::disableForeignKeyConstraints();
         $this->call(OwnerTableSeeder::class);
         $this->call(Collection_Centers_PlasticsTableSeeder::class);
+        $this->call(ProvinciasTableSeeder::class);
+        $this->call(CantonesTableSeeder::class);
+        $this->call(ParroquiasTableSeeder::class);
         Schema::enableForeignKeyConstraints();
 
         // \App\Models\User::factory(10)->create();
-        
+
 
     }
 }
---
+++ database/seeders/ParroquiasTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Parroquia;
+use Illuminate\Database\Seeder;
+
+class ParroquiasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Parroquia :: truncate();
+
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 100; $i++) {
+            Parroquia::create([
+                'nombre' => $faker->sentence,
+            ]);
+        }
+    }
+}
