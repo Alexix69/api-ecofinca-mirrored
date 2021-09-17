aec4247a62dee0d5a3fba915bc9347ae9afc5278
John Vásconez <jovv1994@outlook.com>
2021-09-17 17:51:32 -0500

    Cambios cors

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -181,6 +181,33 @@ class UserController extends Controller
         return response()->json(new UserResource($user), 200);
     }
 
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+//            Cookie::queue(Cookie::forget('token'));
+//            $cookie = Cookie::forget('token');
+//            $cookie->withSameSite('None');
+            return response()->json([
+                "status" => "success",
+                "message" => "User successfully logged out."
+            ], 200)
+                ->withCookie('token', null,
+                config('jwt.ttl'),
+                '/',
+                null,
+                config('app.env') !== 'local',
+                true,
+                false,
+                config('app.env') !== 'local' ? 'None' : 'Lax'
+            );
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return response()->json(["message" => "No se pudo cerrar la sesión."], 500);
+        }
+    }
+
     public function index()
     {
         return response()->json(new UserCollection(User::all()), 200);
